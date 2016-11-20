08b84de8d4cd3d2add1365387e3eb6698a5601d2
Unknown <nhgarcia27@gmail.com>
2016-11-20 18:11:45 -0500

    trying to add dictionary integration

--- roadieQuest/Assets/TypeWriterText.cs
+++ roadieQuest/Assets/TypeWriterText.cs
@@ -1,23 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class TypeWriterText : MonoBehaviour {
 
     public Text textBox;
-    public string[] text = { "Hey, Travis, you guys are on in 15.", "Yeah, whatever.",
-                             "Oh, by the way, tune this guitar before we go up.", "I already did.",
-                             "I switched out the strings. They're gold now.", "Gold? That doesn't really—",
-                             "It matches my hair, really adds to my image up there, Here you go."};
+    public Dictionary<int, string> text = new Dictionary<int, string>
+    {
+            {0, "Hey, Travis, you guys are on in 15."},
+            {1, "Yeah, whatever."},
+            {1, "Oh, by the way, tune this guitar before we go up."},
+            {0, "I already did."},
+            {1, "I switched out the strings. They're gold now."},
+            {0, "Gold? That doesn't really—"},
+            {1, "It matches my hair, really adds to my image up there, Here you go."}
+    };
     public int textDisplay = 0;
 	// Use this for initialization
 	void Start () {
+        textBox = GetComponentInChildren<Text>();
         StartCoroutine(AnimateText());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetButtonDown("Submit"))
+            SkipText();
 	}
 
     //skips the currently displayed text (to be used with a button press) 
@@ -25,7 +34,7 @@ public class TypeWriterText : MonoBehaviour {
     {
         StopAllCoroutines();
         textDisplay++;
-        if(textDisplay > text.Length) //as of now this just loops the text
+        if(textDisplay > text.Count) //as of now this just loops the text
         {
             textDisplay = 0;
         }
@@ -37,8 +46,8 @@ public class TypeWriterText : MonoBehaviour {
     {
         for(int i = 0; i < text[textDisplay].Length + 1; i++)
         {
-            //textDisplayed.text = text[textDisplay].Substring(0, i);
-            yield return new WaitForSeconds(.2f);
+            textBox.text = text[textDisplay].Substring(0, i);
+            yield return new WaitForSeconds(.05f);
         }
     }
 }

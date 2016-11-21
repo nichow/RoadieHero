8843acba6d9e089591e60e6d2242bba476361eca
Unknown <nhgarcia27@gmail.com>
2016-11-21 00:01:18 -0500

    Fuck Dictionaries, made a separate script for head change function

---
+++ roadieQuest/Assets/HeadChange.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadChange : MonoBehaviour {
+
+    public Sprite roadieHead;
+    public Sprite travisHead;
+    int[] personTalking = { 0, 1, 1, 0, 1, 0, 1 };
+    int personTracker;
+
+    // Use this for initialization
+    void Start () {
+        personTracker = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Submit"))
+        {
+            personTracker++;
+        }
+        if (personTalking[personTracker] == 0)
+        {
+            GetComponent<UnityEngine.UI.Image>().sprite = roadieHead;
+        }
+        else
+        {
+            GetComponent<UnityEngine.UI.Image>().sprite = travisHead;
+        }
+    }
+
+}
--- roadieQuest/Assets/TypeWriterText.cs
+++ roadieQuest/Assets/TypeWriterText.cs
@@ -1,21 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-using System.Collections.Generic;
+using System.Collections.Specialized;
 
 public class TypeWriterText : MonoBehaviour {
 
     public Text textBox;
-    public Dictionary<int, string> text = new Dictionary<int, string>
-    {
-            {0, "Hey, Travis, you guys are on in 15."},
-            {1, "Yeah, whatever."},
-            {1, "Oh, by the way, tune this guitar before we go up."},
-            {0, "I already did."},
-            {1, "I switched out the strings. They're gold now."},
-            {0, "Gold? That doesn't really—"},
-            {1, "It matches my hair, really adds to my image up there, Here you go."}
-    };
+    public string[] text =
+        {"Hey, Travis, you guys are on in 15.",
+         "Yeah, whatever.",
+         "Oh, by the way, tune this guitar before we go up.",
+         "Huh? I already did.",
+         "I switched out the strings. They're gold now.",
+         "Gold? That doesn't really—",
+         "It matches my hair, really adds to my image up there, Here you go."};
     public int textDisplay = 0;
 	// Use this for initialization
 	void Start () {
@@ -34,7 +32,7 @@ public class TypeWriterText : MonoBehaviour {
     {
         StopAllCoroutines();
         textDisplay++;
-        if(textDisplay > text.Count) //as of now this just loops the text
+        if(textDisplay > text.Length) //as of now this just loops the text
         {
             textDisplay = 0;
         }
@@ -44,7 +42,7 @@ public class TypeWriterText : MonoBehaviour {
     //does typewriter text
     IEnumerator AnimateText()
     {
-        for(int i = 0; i < text[textDisplay].Length + 1; i++)
+        for(int i = 0; i < text[textDisplay].Length; i++)
         {
             textBox.text = text[textDisplay].Substring(0, i);
             yield return new WaitForSeconds(.05f);
